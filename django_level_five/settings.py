58c79688a87ed9fe7ab29ce791e9265a6ca15ad5
sital1331 <stl.mohapatra@gmail.com>
2023-07-10 14:14:31 +0530

    adding production env file

--- django_level_five/settings.py
+++ django_level_five/settings.py
@@ -5,7 +5,7 @@ IS_PRODUCTION = os.environ.get('IS_PRODUCTION')
 
 
 if IS_PRODUCTION:
-    from conf.production.settings import *
+    from .conf.production.settings import *
 else:
     
     from .conf.development.settings import *
